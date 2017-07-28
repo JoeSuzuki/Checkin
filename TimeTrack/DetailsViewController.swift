9c63485bd1e3a911d69059cbed26779762d54f1e
Joe Suzuki <joesuzuki516@gmail.com>
2017-07-28 13:06:21 -0400

    finished group adding/ firebase screens

---
+++ TimeTrack/DetailsViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DetailsViewController.swift
+//  TimeTrack
+//
+//  Created by Joe Suzuki on 7/28/17.
+//  Copyright © 2017 JoeSuzuki. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class DetailsViewController: UIViewController {
+    
+    @IBOutlet weak var groupNameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextView!
+    
+    // hides keyboard
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    // hides keyboard when pressing return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        groupNameTextField.resignFirstResponder()
+        descriptionTextField.resignFirstResponder()
+        return(true)
+    }
+    
+    var ref: DatabaseReference!
+    let userID = Auth.auth().currentUser!.uid
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = Database.database().reference().child("users").child(userID).child("groups").child("personal groups").childByAutoId()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addButton(_ sender: UIButton) {
+    self.ref?.child("group name").updateChildValues(["name": groupNameTextField.text as Any])
+    self.ref?.child("description").updateChildValues(["description": descriptionTextField.text as Any])
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    //self.ref?.child("users").child(userID).child("groups").child("personal groups").childByAutoId().updateChildValues(["address": searchTextField.text])
+    //self.ref?.child("users").child(userID).child("groups").updateChildValues(["personal groups": "ppppp"])
+    
+
+
+}
---
+++ TimeTrack/GroupLocationViewController.swift
@@ -0,0 +1,51 @@
+//
+//  GroupLocationViewController.swift
+//  TimeTrack
+//
+//  Created by Joe Suzuki on 7/28/17.
+//  Copyright © 2017 JoeSuzuki. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class GroupLocationViewController: UIViewController {
+
+    //screen 1
+    @IBOutlet weak var locationDisplay: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+   // hides keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    // hides keyboard when pressing return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.resignFirstResponder()
+        return(true)
+    }
+    var ref: DatabaseReference!
+    let userID = Auth.auth().currentUser!.uid
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        ref = Database.database().reference().child("users").child(userID).child("groups").child("personal groups").childByAutoId()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func searchButton(_ sender: UIButton) {
+        locationDisplay.text = searchTextField.text
+        self.ref?.child("location").updateChildValues(["location": searchTextField.text as Any])
+        }
+ 
+
+    //self.ref?.child("users").child(userID).child("groups").child("personal groups").childByAutoId().updateChildValues(["address": searchTextField.text])
+    //self.ref?.child("users").child(userID).child("groups").updateChildValues(["personal groups": "ppppp"])
+
+   
+}
