fb82e3636c732d3b125631d1d20976692bd1e334
Joe Suzuki <joesuzuki516@gmail.com>
2017-08-30 18:24:16 -0400

    create group map feature

--- TimeTrack/CreateGroupsViewController.swift
+++ TimeTrack/CreateGroupsViewController.swift
@@ -9,11 +9,14 @@
 import Foundation
 import UIKit
 import Eureka
+import MapKit
+import CoreLocation
 
-class CreateGroupsViewController: FormViewController {
+class CreateGroupsViewController: FormViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var locationManager = CLLocationManager()
         form +++ Section("Section1")
             <<< TextRow(){ row in
                 row.title = "Text Row"
@@ -27,6 +30,20 @@ class CreateGroupsViewController: FormViewController {
             <<< DateRow(){
                 $0.title = "Date Row"
                 $0.value = Date(timeIntervalSinceReferenceDate: 0)
-        }
+            }
+            <<< LocationRow(){
+                $0.title = "LocationRow"
+                $0.value = CLLocation(latitude: -34.91, longitude: -56.1646)
+            }
+            
+    	// Enables the navigation accessory and stops navigation when a disabled row is encountered
+    navigationOptions = RowNavigationOptions.Enabled.union(.StopDisabledRow)
+    // Enables smooth scrolling on navigation to off-screen rows
+    animateScroll = true
+    // Leaves 20pt of space between the keyboard and the highlighted row after scrolling to an off screen row
+    rowKeyboardSpacing = 20
+    }
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation // note that locations is same as the one in the function declaration
     }
 }
